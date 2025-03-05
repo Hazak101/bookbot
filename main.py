067de6323a90f3aff990f9cf5c1f819cb12bd5cc
Hazak101 <ericolbrich@hotmail.com>
2025-03-05 00:20:20 -0500

    book data correction

--- main.py
+++ main.py
@@ -2,7 +2,15 @@ def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
     return file_contents
-def main():
-    print(get_book_text("books/frankenstein.txt"))
 
-main()  
+def counting_words(file_contents):
+    words = file_contents.split()
+    return len(words)
+
+def main():
+    path_to_file = 'books/frankenstein.txt'  # Specify the path to your book file
+    book_text = get_book_text(path_to_file)
+    num_words = counting_words(book_text)
+    print(f"{num_words} words found in the document")
+
+main()
